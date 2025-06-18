922c93bc91966c96b3b7e7df27d9897e9b1d5bbb
arav20603 <dakshandaravind28@gmail.com>
2025-06-18 15:15:22 +0530

    Added CV using G-drive

--- portfolio/src/constants/links.ts
+++ portfolio/src/constants/links.ts
@@ -4,4 +4,5 @@ export const links = {
   gitHub: 'https://www.github.com/Arav20603',
   linkedIn: 'https://www.linkedin.com/in/aravind-dakshan-d-ab117a2a3/',
   X: 'https://www.X.com/Syndicate0ne/',
+  CV: 'https://lnk.ink/Vwi3z'
 }
--- portfolio/src/pages/Hero.tsx
+++ portfolio/src/pages/Hero.tsx
@@ -1,4 +1,5 @@
 import { images } from "../constants/images"
+import { links } from "../constants/links"
 import '../utils/Hero.css'
 
 const Hero = () => {
@@ -23,7 +24,7 @@ const Hero = () => {
 
         {/* btn - section */}
         <div className="btn flex gap-10 mt-3 font-semibold ml-5">
-          <a href="#" className="p-2 bg-blue-700 px-7 text-center rounded-xl hover:bg-blue-800 transition-all delay-75 duration-200">View Resume</a>
+          <a href={links.CV} className="p-2 bg-blue-700 px-7 text-center rounded-xl hover:bg-blue-800 transition-all delay-75 duration-200">View Resume</a>
           <a href="#contact" className="p-2 bg-indigo-800 px-7 text-center rounded-xl hover:bg-indigo-900 transition-all delay-75 duration-200">Contact Me</a>
         </div>
       </div>
